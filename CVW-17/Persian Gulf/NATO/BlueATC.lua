45d1d8c15aa3ced084e273cdda6fdd78694feab5
Circuit <navalairsimulationsgroup@gmail.com>
2026-05-08 21:51:03 -0400

    fixed atc

--- CVW-17/Persian Gulf/NATO/BlueATC.lua
+++ CVW-17/Persian Gulf/NATO/BlueATC.lua
@@ -1,7 +1,8 @@
 
 
 FC_Al_Minhad = FLIGHTCONTROL:New(AIRBASE.PersianGulf.Al_Minhad_AFB, 250.1, radio.modulation.AM, "")
-FC_Al_Minhad:SetSRS("", "male", "en-US")
+FC_Al_Minhad:SetSRSPilot("", "male", "en-US")
+FC_Al_Minhad:SetSRSTower("", "female", "en-US")
 NASG_TTS:Use(FC_Al_Minhad.msrsPilot, "Al Minhad Pilot", "Nathan", 200, 1.0)
 NASG_TTS:Use(FC_Al_Minhad.msrsTower, "Al Minhad Tower", "Zoe",200, 1.0)
 FC_Al_Minhad:SetParkingGuardStatic("ALParkGuard")
