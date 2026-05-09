a331b1fc948cc39ca7da63f16e9af5b0138576af
frankiep95 <38260710+frankiep95@users.noreply.github.com>
2026-05-08 21:54:58 -0400

    blue ranges touches

--- CVW-17/Persian Gulf/Training/Blue_Ranges.lua
+++ CVW-17/Persian Gulf/Training/Blue_Ranges.lua
@@ -2,22 +2,26 @@
 
 local BombTargetGoodHitDistance = 25
 
-AzZafrahRange = RANGE:New("Az Zafrah Range" , coalition.side.BLUE)
-AzZafrahRange:SetSRS(SRS_PATH,SRS_PORT,coalition.side.BLUE,251, radio.modulation.AM,1.0,GOOGLE_CREDS)
-AzZafrahRange:SetSRSRangeControl(251.5, radio.modulation.AM, SRS_VOICES.Female.en_US_Wavenet_G, "en-US", "female", "Pagan Radio Relay")
-AzZafrahRange:SetSRSRangeInstructor(251, radio.modulation.AM, SRS_VOICES.Male.en_US_Wavenet_J, "en-US", "male", "Pagan Radio Relay")
+AzZafrahRange = RANGE:New("Az Zafrah Range", coalition.side.BLUE)
+
+AzZafrahRange:SetSRS(SRS_PATH, SRS_PORT, coalition.side.BLUE, 251, radio.modulation.AM, 1.0, nil)
+AzZafrahRange:SetSRSRangeControl(251.5, radio.modulation.AM, "Zoe", "en-US", "female", "Pagan Radio Relay")
+AzZafrahRange:SetSRSRangeInstructor(251, radio.modulation.AM, "Nathan", "en-US", "male", "Pagan Radio Relay")
+
+NASG_TTS:Use(AzZafrahRange.controlmsrs, "Az Zafrah Range Control", "Zoe", 200, 1.0)
+NASG_TTS:Use(AzZafrahRange.instructmsrs, "Az Zafrah Range Instructor", "Nathan", 200, 1.0)
+
 AzZafrahRange:SetFunkManOn(10042, "127.0.0.1")
 
-local fouldist = AzZafrahRange:GetFoullineDistance("strafe 1","foul line 1")
+local fouldist = AzZafrahRange:GetFoullineDistance("strafe 1", "foul line 1")
 
-AzZafrahRange:AddStrafePit("strafe 1",3000,300,180,360,true,20,fouldist)
+AzZafrahRange:AddStrafePit("strafe 1", 3000, 300, 180, 360, true, 20, fouldist)
 
-local AzZafrahBombTargets ={
-"circle 1",
+local AzZafrahBombTargets = {
+    "circle 1",
 }
 
-
-AzZafrahRange:AddBombingTargets(AzZafrahBombTargets,BombTargetsGoodHitDistance)
+AzZafrahRange:AddBombingTargets(AzZafrahBombTargets, BombTargetGoodHitDistance)
 
 local azzafrah_range_zone = ZONE:FindByName("AZ ZAFRAH Range")
 AzZafrahRange:SetRangeZone(azzafrah_range_zone)
@@ -25,14 +29,12 @@ AzZafrahRange:SetRangeZone(azzafrah_range_zone)
 function AzZafrahRange:OnAfterEnterRange(From, Event, To, player)
     BASE:I("Entering Range")
     local text = string.format("fix\n%s has entered the %s.\n", player.playername, self.rangename)
-    -- HypeMan.sendBotMessage(text)
     dcsbot.sendBotMessage(text)
 end
 
 function AzZafrahRange:OnAfterExitRange(From, Event, To, player)
-    BASE:I("Exting Range")
+    BASE:I("Exiting Range")
     local text = string.format("fix\n%s has exited %s.\n", player.playername, self.rangename)
-    -- HypeMan.sendBotMessage(text)
     dcsbot.sendBotMessage(text)
 end
 
@@ -44,13 +46,11 @@ function AzZafrahRange:OnAfterStrafeResult(From, Event, To, player, result)
     self:T("OnAfterStrafeResult")
 end
 
-
 AzZafrahRange:SetSoundfilesPath(RANGESOUNDFOLDER)
 AzZafrahRange:SetTargetSheet(TARGETSHEETSTRAFELOCATION)
 AzZafrahRange:TrackRocketsOFF()
 AzZafrahRange:SetMessagesOFF()
 AzZafrahRange:SetAutosaveOn()
--- PaganRange:SetBombtrackThreshold(55)
 AzZafrahRange:SetBombtrackThreshold(UTILS.NMToKiloMeters(100))
 
 -- Start range.
