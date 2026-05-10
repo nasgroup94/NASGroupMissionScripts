f0d80cb3b16ba91b3eb30b7bdc842636fc24f23e
frankiep95 <38260710+frankiep95@users.noreply.github.com>
2026-05-10 19:40:20 -0400

    SEAD range has basic functionality

--- CVW-17/Persian Gulf/Training/SEADRangeIADS.lua
+++ CVW-17/Persian Gulf/Training/SEADRangeIADS.lua
@@ -1,6 +1,7 @@
 SEADRangeIADS = SkynetIADS:create('SEAD RANGE IADS')
 SEADRangeIADS:addSAMSitesByPrefix("srsa")
 SEADRangeIADS:addEarlyWarningRadarsByPrefix("ewsr")
+SEADRangeIADS.IADSStatus = true
 
 
 local sa2Group = SPAWN:New("srsa2")
@@ -11,25 +12,64 @@ local sa11Group = SPAWN:New("srsa11")
 
 local RangeMenu = MENU_COALITION:New(coalition.side.BLUE, "SEAD Range")
 
-local function clearRange(group)
-    group:Destroy()
+local theGroup = nil
+local RangeCommands = {}
+local ClearRangeCommand = nil
+
+local ActivateGroup
+local clearRange
+local buildRangeCommands
+local removeRangeCommands
+
+removeRangeCommands = function()
+    for _, command in pairs(RangeCommands) do
+        if command then
+            command:Remove()
+        end
+    end
+
+    RangeCommands = {}
 end
 
-local function ActivateGroup(group)
-    group:Spawn()
+buildRangeCommands = function()
+    RangeCommands = {}
 
-    MESSAGE:New("SA-2 Activated", 10):ToAll()
-
-    RangeMenu:Remove()
-
-    local deactivate = MENU_COALITION:New("SEAD Range",coalition.side.blue)
-    MENU_COALITION_COMMAND:New(coalition.side.Blue,"Clear Range",clearRange(group))
-
-
+    RangeCommands[#RangeCommands + 1] = MENU_COALITION_COMMAND:New(coalition.side.BLUE, "SA-2", RangeMenu, ActivateGroup, sa2Group)
+    RangeCommands[#RangeCommands + 1] = MENU_COALITION_COMMAND:New(coalition.side.BLUE, "SA-5", RangeMenu, ActivateGroup, sa5Group)
+    RangeCommands[#RangeCommands + 1] = MENU_COALITION_COMMAND:New(coalition.side.BLUE, "SA-6", RangeMenu, ActivateGroup, sa6Group)
+    RangeCommands[#RangeCommands + 1] = MENU_COALITION_COMMAND:New(coalition.side.BLUE, "SA-10", RangeMenu, ActivateGroup, sa10Group)
+    RangeCommands[#RangeCommands + 1] = MENU_COALITION_COMMAND:New(coalition.side.BLUE, "SA-11", RangeMenu, ActivateGroup, sa11Group)
 end
 
-MENU_COALITION_COMMAND:New(coalition.side.BLUE, "SA-2", RangeMenu, ActivateGroup(sa2Group))
-MENU_COALITION_COMMAND:New(coalition.side.BLUE, "SA-5", RangeMenu, ActivateGroup(sa5Group))
-MENU_COALITION_COMMAND:New(coalition.side.BLUE, "SA-6", RangeMenu, ActivateGroup(sa6Group))
-MENU_COALITION_COMMAND:New(coalition.side.BLUE, "SA-10", RangeMenu, ActivateGroup(sa10Group))
-MENU_COALITION_COMMAND:New(coalition.side.BLUE, "SA-11", RangeMenu, ActivateGroup(sa11Group))
+clearRange = function(group)
+    SEADRangeIADS:deactivate()
+    if group then
+        group:Destroy()
+    end
+
+    if ClearRangeCommand then
+        ClearRangeCommand:Remove()
+        ClearRangeCommand = nil
+    end
+
+    theGroup = nil
+
+    buildRangeCommands()
+
+    MESSAGE:New("SEAD Range Cleared", 10):ToAll()
+end
+
+ActivateGroup = function(spawnObject)
+    local spawnedGroup = spawnObject:Spawn()
+    SEADRangeIADS:activate()
+
+    MESSAGE:New(spawnedGroup:GetName() .. " Activated", 10):ToAll()
+
+    removeRangeCommands()
+
+    theGroup = spawnedGroup
+
+    ClearRangeCommand = MENU_COALITION_COMMAND:New(coalition.side.BLUE, "Clear Range", RangeMenu, clearRange, theGroup)
+end
+
+buildRangeCommands()
